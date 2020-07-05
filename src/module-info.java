5bdab427492fed99d2d420d9f26da6bc06b8a6a0
valchevgd <valchevgd@gmail.com>
2020-07-05 19:11:31 +0300

    add activation and java mail jars, update module-info

--- src/module-info.java
+++ src/module-info.java
@@ -4,6 +4,8 @@ module EmailClient {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.web;
+    requires activation;
+    requires java.mail;
 
     opens com.github.valchevgd;
     opens com.github.valchevgd.view;
